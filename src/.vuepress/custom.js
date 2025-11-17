1af04c357ce68559f469a9f88e06851dfd8df018
Robin Zheng <1487805+zhengr@users.noreply.github.com>
2025-11-17 08:39:33 +0800

    Update custom.js

--- src/.vuepress/custom.js
+++ src/.vuepress/custom.js
@@ -34,34 +34,14 @@ let mainConfig = {
         name: "首页",
         link: "/Issue-Blog-With-Github-Action"
       },
-      {
-        name: "Github",
-        link: "https://github.com/zhengr"
-      },
-      {
-        name: "CV",
-        link: "/Issue-Blog-With-Github-Action/cv.html"
-      }
-    ],
     headTitle: ["暮春早夏的月亮", "原是情人的月亮，不比秋冬是诗人的月亮"],
     friendLinks: [
-      {
-        name: "watercow",
-        link: "https://github.com/watercow/watercow.github.io/issues"
-      },
-      {
-        name: "ycjgg",
-        link: "https://ycjgg.github.io"
-      }
     ],
     extraFooters: [
       {
-        title: "ABOUT",
-        text: "@ 蜀 ICP 备 19000509 号 - 1",
-        link: "https://beian.miit.gov.cn/"
       }
     ],
-    pageCount: true
+    pageCount: false
   },
 
   head: [
